c390f1eaccbdb380da7752bef091f8cc1d5d9a0c
David Regan <83370854+davidrobertregan@users.noreply.github.com>
2021-09-08 09:59:18 -0500

    finish core

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,15 @@ import ListingsContainer from "./ListingsContainer";
 function App() {
 
 const [listings, setListings] = useState([])
+const [defaultListings, setDefaultListings] = useState([])
 
 useEffect(() => {
   fetch('http://localhost:6001/listings')
     .then(resp => resp.json())
-    .then(listings => setListings(listings))
+    .then(listings => {
+      setListings(listings)
+      setDefaultListings(listings)
+    })
 }, [])
 
 function deleteListing(id){
@@ -18,9 +22,20 @@ function deleteListing(id){
   setListings(newListings)
 }
 
+function setSearchFilter(input){
+  console.log('search!')
+  const newListings = listings.filter(l => l.description.toLowerCase().startsWith(input.toLowerCase()))
+  
+  if(input === "") {
+    setListings(defaultListings)
+  }
+  else {setListings(newListings)}
+}
+
+
   return (
     <div className="app">
-      <Header />
+      <Header setSearchFilter={setSearchFilter} />
       <ListingsContainer listings={listings} deleteListing={deleteListing}/>
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Search from "./Search";
 
-function Header() {
+function Header( {setSearchFilter}) {
   return (
     <header>
       <h1>
@@ -10,7 +10,7 @@ function Header() {
         </span>
         gregslist
       </h1>
-      <Search />
+      <Search setSearchFilter={setSearchFilter}/>
     </header>
   );
 }
